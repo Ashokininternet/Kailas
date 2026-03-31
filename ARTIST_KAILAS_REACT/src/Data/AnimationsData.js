ff859e0ed14b94c971f4d479f185f24e9bfa8ebc
Ashok S.R <rajalekshmykomalam983@gmail.com>
2026-03-31 11:45:55 +0530

    Update 11
    Just added MP4 folder and some other fixes

--- ARTIST_KAILAS_REACT/src/Data/AnimationsData.js
+++ ARTIST_KAILAS_REACT/src/Data/AnimationsData.js
@@ -1,34 +1,41 @@
 export const gridData = [
     {
         id: crypto.randomUUID(),
-        img: "GIF/Kicking animation.mp4",
+        img: "MP4/Kicking animation.mp4",
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/KJ1.mp4",
+        img: "MP4/KJ1.mp4",
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/PunchingBag.mp4",
+        img: "MP4/PunchingBag.mp4",
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/SuperSayan.mp4",
+        img: "MP4/SuperSayan.mp4",
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/FlameingF09F918A.mp4"
+        img: "MP4/FlameingF09F918A.mp4"
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/Telephonic.mp4"
+        img: "MP4/Telephonic.mp4"
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/Kjstomipbomb.mp4"
+        img: "MP4/Kjstomipbomb.mp4"
     },
     {
         id: crypto.randomUUID(),
-        img: "GIF/Fight.mp4"
+        img: "MP4/Fight.mp4"
+    },
+      {
+        id: crypto.randomUUID(),
+        img: "MP4/Luffy GumGum Gatling.mp4"
+    },  {
+        id: crypto.randomUUID(),
+        img: "MP4/StickMan space fight anmination.mp4"
     },
 ];
